544e464f6fee2006a4f85cd0e326f976a764177b
Michael <m-silkov@inbox.ru>
2023-01-15 21:59:59 +0300

    feat: create controllers and routes for users and testuser

--- app.js
+++ app.js
@@ -3,17 +3,14 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const { PORT = 3000 } = process.env;
-
+const { PORT = 3000, BASE_PATH } = process.env;
 const app = express();
-app.use(bodyParser.json()); // для собирания JSON-формата
-app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 
-mongoose.connect("mongodb://localhost:27017/mestodb", {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useFindAndModify: false,
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb://localhost:27017/mestodb");
 
 app.use("/users", require("./routes/users"));
 
@@ -21,4 +18,5 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
 	console.log(`App listening on port ${PORT}`);
+	console.log(BASE_PATH);
 });
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,23 @@
 const router = require("express").Router();
 const User = require("../models/user");
 
-router.get('/', (req, res) => {
-  User.find({})
-    .then(users => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+router.get("/", (req, res) => {
+	User.find({})
+		.then((users) => res.send({ data: users }))
+		.catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+});
+
+router.get("/:_id", (req, res) => {
+	User.findById(req.params._id)
+		.then((users) => res.send({ data: users }))
+		.catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+});
+
+router.post("/", (req, res) => {
+	const { name, about, avatar } = req.body;
+	User.create({ name, about, avatar })
+		.then((user) => res.send({ data: user }))
+		.catch(() => res.status(500).send({ message: "Произошла ошибка" }));
 });
 
 module.exports = router;
