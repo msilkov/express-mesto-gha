8f0ec1899ec06bec1dc25aea81664485c0cc5789
Michael <m-silkov@inbox.ru>
2023-01-18 00:37:11 +0300

    fix: fix errors responses

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.use("/users", require("./routes/users"));
 app.use("/cards", require("./routes/cards"));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("*", (req, res) => {
+	res.status(404).send({ message: "Страница не найдена." });
+});
+
 app.listen(PORT, () => {
 	console.log(`App listening on port ${PORT}`);
 });
