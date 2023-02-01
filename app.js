670a533497932c389468f577a7ff0762e8a8c535
Michael <m-silkov@inbox.ru>
2023-02-01 21:16:02 +0300

    :fix: add joi name

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.post(
     body: Joi.object().keys({
       email: Joi.string().required().email(),
       password: Joi.string().required().min(8),
+      name: Joi.string().min(2).max(30),
     }).unknown(true),
   }),
   createUser,
