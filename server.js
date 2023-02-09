551645733d3769126f91d455681daaf53c808075
Michael <m-silkov@inbox.ru>
2023-02-09 22:49:11 +0300

    fix: fix cors

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const app = require('./app');
 const { cors } = require('./middlewares/cors');
+const { allowedCors } = require('./utils/utils.js');
 
 const { PORT = 3000 } = process.env;
 
-app.use(cors);
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.end();
+  }
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
