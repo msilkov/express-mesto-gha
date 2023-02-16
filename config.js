e8fb136b1d95fba5125de3ed719f2985ab8f4b87
Michael <m-silkov@inbox.ru>
2023-02-16 22:42:42 +0300

    fix: fix review errs

---
+++ config.js
@@ -0,0 +1,10 @@
+const { NODE_ENV, JWT_SECRET, PORT = 3000 } = process.env;
+
+module.exports = {
+  NODE_ENV, JWT_SECRET, PORT,
+};
+
+// logout route
+// refactor
+// google fix
+// connect mogo compas for serv
