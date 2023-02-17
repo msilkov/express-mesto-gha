eae769293f761953450d95b180e54b42bde69902
Michael <m-silkov@inbox.ru>
2023-02-17 20:26:10 +0300

    fix: fix jwt env

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -19,28 +19,28 @@ const auth = (req, res, next) => {
   return next();
 };
 
-// const YOUR_JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2U2OTRlODZjNjFkYWI3YTMzYmZiZGUiLCJpYXQiOjE2NzY2NTM5OTMsImV4cCI6MTY3NzI1ODc5M30.O1mG12_jQUDNFl0E8e0L-sYydEcdNpFU6JNY5hQWwDk'; // вставьте сюда JWT, который вернул публичный сервер
-// const SECRET_KEY_DEV = '222f7ea40eed03f3363eb8d1d7728dd674331a19bdc01759ff157127458001a6'; // вставьте сюда секретный ключ для разработки из кода
-// try {
-//   const payloadCh = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
-//   console.log('\x1b[31m%s\x1b[0m', `
-// Надо исправить. В продакшне используется тот же
-// секретный ключ, что и в режиме разработки.
-// `);
-// } catch (err) {
-//   if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
-//     console.log(
-//       '\x1b[32m%s\x1b[0m',
-//       'Всё в порядке. Секретные ключи отличаются',
-//     );
-//   } else {
-//     console.log(
-//       '\x1b[33m%s\x1b[0m',
-//       'Что-то не так',
-//       err,
-//     );
-//   }
-// }
+const YOUR_JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2U2OTRlODZjNjFkYWI3YTMzYmZiZGUiLCJpYXQiOjE2NzY2NTQyMjcsImV4cCI6MTY3NzI1OTAyN30.omEvCu8yNfAg-q8ObdhwIkgWjaGrbtPSJrIQqEfa7Y8'; // вставьте сюда JWT, который вернул публичный сервер
+const SECRET_KEY_DEV = '222f7ea40eed03f3363eb8d1d7728dd674331a19bdc01759ff157127458001a6'; // вставьте сюда секретный ключ для разработки из кода
+try {
+  const payloadCh = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
+  console.log('\x1b[31m%s\x1b[0m', `
+Надо исправить. В продакшне используется тот же
+секретный ключ, что и в режиме разработки.
+`);
+} catch (err) {
+  if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
+    console.log(
+      '\x1b[32m%s\x1b[0m',
+      'Всё в порядке. Секретные ключи отличаются',
+    );
+  } else {
+    console.log(
+      '\x1b[33m%s\x1b[0m',
+      'Что-то не так',
+      err,
+    );
+  }
+}
 
 module.exports = {
   auth,
